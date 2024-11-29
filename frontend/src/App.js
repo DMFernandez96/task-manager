012ac9260b5042af1e1f0749e6a144e1a12d7880
Manuela Fernandez <manu.fer.0396@gmail.com>
2024-11-28 22:31:30 -0300

    Create components modal for edit and delete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./css/EditModal.css";
 import "./App.css";
-import Header from "./Header";
-import Footer from "./Footer";
-import TaskSection from "./TaskSection";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import TaskSection from "./components/TaskSection";
+import EditModal from "./components/EditModal";
+import ConfirmDeleteModal from "./components/ConfirmDeleteModal";
 
 function App() {
   const [tareas, setTareas] = useState([]);
+  const [tareaSeleccionada, setTareaSeleccionada] = useState(null);
+	const [tareaEliminar, setTareaEliminar] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:3001/api/tareas")
@@ -14,12 +21,30 @@ function App() {
       .catch((error) => console.error("Error al obtener tareas:", error));
   }, []);
 
+
   const eliminarTarea = (id) => {
-    console.log(`Eliminar tarea con id: ${id}`);
+    const tarea = tareas.find((t) => t.id_tarea === id);
+    setTareaEliminar(tarea);
   };
+	 
+	const confirmarEliminacion = (id) => {
+     console.log(`Tarea con ID ${id} eliminada`);
+     setTareas(tareas.filter((t) => t.id_tarea !== id));
+     setTareaEliminar(null); 
+   };
 
   const editarTarea = (id) => {
-    console.log(`Editar tarea con id: ${id}`);
+    const tarea = tareas.find((t) => t.id_tarea === id);
+    setTareaSeleccionada(tarea);
+  };
+
+  const actualizarTarea = (tareaActualizada) => {
+    setTareas((prevTareas) =>
+      prevTareas.map((t) =>
+        t.id_tarea === tareaActualizada.id_tarea ? tareaActualizada : t
+      )
+    );
+    setTareaSeleccionada(null);
   };
 
   return (
@@ -33,6 +58,22 @@ function App() {
         />
       </main>
       <Footer />
+
+      {tareaSeleccionada && (
+        <EditModal
+          tarea={tareaSeleccionada}
+          onClose={() => setTareaSeleccionada(null)}
+          onUpdate={actualizarTarea}
+        />
+      )}
+
+      {tareaEliminar && (
+        <ConfirmDeleteModal
+          tarea={tareaEliminar}
+          onClose={() => setTareaEliminar(null)} 
+          onConfirm={confirmarEliminacion} 
+        />
+      )}
     </div>
   );
 }
